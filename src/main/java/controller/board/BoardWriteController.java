ae057f3de57cdd75cb19310097a777897b9c138f
김효정 <136438704+KHJ2620@users.noreply.github.com>
2024-06-25 16:37:42 +0900

    add boardwrite

--- src/main/java/controller/board/BoardWriteController.java
+++ src/main/java/controller/board/BoardWriteController.java
@@ -7,12 +7,18 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import model.vo.User;
 
 @WebServlet("/board/write")
 public class BoardWriteController extends HttpServlet{
 	
 	@Override
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		User authUser = (User) request.getSession().getAttribute("authUser");
+		if (authUser == null) {
+			response.sendRedirect(request.getContextPath()+"/login");
+			return;
+		}
 		
 		request.getRequestDispatcher("/WEB-INF/view/board/write.jsp").forward(request, response);
 	}
