04e238332b98dd32cc83956750288fc8d51439de
김효정 <136438704+KHJ2620@users.noreply.github.com>
2024-06-26 17:36:19 +0900

    add board category

--- src/main/java/controller/board/BoardController.java
+++ src/main/java/controller/board/BoardController.java
@@ -17,6 +17,29 @@ public class BoardController extends HttpServlet {
 	@Override
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		try {
+			String category = null;
+
+            if (request.getParameter("home") != null) {
+                category = "홈";
+            } else if (request.getParameter("campingLog") != null) {
+                category = "캠핑로그";
+            } else if (request.getParameter("campingPhoto") != null) {
+                category = "캠핑한컷";
+            } else if (request.getParameter("campingTalk") != null) {
+                category = "캠핑톡톡";
+            } else if (request.getParameter("questions") != null) {
+                category = "궁금해요";
+            } else if (request.getParameter("articles") != null) {
+                category = "아티클";
+            }
+            
+			if (category == null || category.isEmpty()) {
+				category = "홈";
+			}
+			
+			
+			request.setAttribute("category", category);
+			
 			int p = request.getParameter("p")==null ? 1 : Integer.parseInt(request.getParameter("p"));
 			int size = 10;
 			int start = size*(p-1) +1;
@@ -26,7 +49,7 @@ public class BoardController extends HttpServlet {
 			int count = boardDao.countAll();
 			int totalPages = count/size + (count%size > 0 ? 1: 0);
 			
-			request.setAttribute("boards", boardDao.findAll(start, end));
+			request.setAttribute("boards", boardDao.findByCategory(category,  start, end));
 			request.setAttribute("totalPages",totalPages);
 			request.setAttribute("currentPage", p);
 			
